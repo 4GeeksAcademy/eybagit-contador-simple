02700291bde99499b06c5cfe311206c0374d85fb
Elkin Botero <147097939+eybagit@users.noreply.github.com>
2025-07-15 02:10:38 +0000

    Ajustes

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/index.css'
 
-
 import SecondsCounter from './components/SecondsCounter.jsx'
 
 
@@ -11,7 +10,7 @@ let segundos = 0
 
 setInterval(() => {
   ReactDOM.createRoot(document.getElementById('root')).render(
-      <SecondsCounter segundos={segundos} enCuentaRegresiva={true} />
+      <SecondsCounter segundos={segundos} />
   )
   segundos++
 }, 1000)
